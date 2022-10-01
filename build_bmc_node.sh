9cd5c74f751e7d067916586beb639f174b235dbd
崔浩鑫 <cuihaoxin@cuihaoxindeMacBook-Pro.local>
2022-10-01 18:03:07 +0800

    feat(modify): modify

--- build_bmc_node.sh
+++ build_bmc_node.sh
@@ -1,3 +1,3 @@
 block_height=23456
 
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-X google.golang.org/protobuf/reflect/protoregistry.conflictPolicy=warn" -o bmc_${block_height} ./cmd/geth/main.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-X google.golang.org/protobuf/reflect/protoregistry.conflictPolicy=warn" -o bmc_23456 ./cmd/geth/main.go
